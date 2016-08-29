85b4845f1ab87d535d4ff63e1970aa16bd3ec2e9
Arcao <arcao@arcao.com>
2016-08-29 12:29:36 +0200

    Updated oauth example to use latest Geocaching API an Scribe library

--- oauth/src/main/java/com/arcao/geocaching/api/example/oauth/GeocachingMain.java
+++ oauth/src/main/java/com/arcao/geocaching/api/example/oauth/GeocachingMain.java
@@ -1,91 +1,94 @@
 package com.arcao.geocaching.api.example.oauth;
 
 import com.arcao.geocaching.api.GeocachingApi;
+import com.arcao.geocaching.api.LiveGeocachingApi;
 import com.arcao.geocaching.api.configuration.impl.DefaultStagingGeocachingApiConfiguration;
 import com.arcao.geocaching.api.data.Geocache;
 import com.arcao.geocaching.api.example.oauth.provider.GeocachingOAuthProvider;
-import com.arcao.geocaching.api.impl.LiveGeocachingApi;
-import org.scribe.builder.ServiceBuilder;
-import org.scribe.model.Token;
-import org.scribe.model.Verifier;
-import org.scribe.oauth.OAuthService;
+import com.arcao.geocaching.api.exception.GeocachingApiException;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth1AccessToken;
+import com.github.scribejava.core.model.OAuth1RequestToken;
+import com.github.scribejava.core.oauth.OAuth10aService;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class GeocachingMain {
-	// Ask Groundspeak for your own keys, see: http://www.geocaching.com/live/apidevelopers/
-	// These are my staging keys. Staging server is slow with old database data. Do not use it for
-	// production use. This is only for testing purpose!!!
-	public static final String OAUTH_CONSUMER_KEY = "9C7552E1-3C04-4D04-A395-230D8931E494";
-	public static final String OAUTH_CONSUMER_SECRET = "DA7CC147-7B5B-4423-BCB4-D0C03E2BF685";
+public final class GeocachingMain {
+    // Ask Groundspeak for your own keys, see: http://www.geocaching.com/live/apidevelopers/
+    // These are my staging keys. Staging server is slow with old database data. Do not use it for
+    // production use. This is only for testing purpose!!!
+	private static final String OAUTH_CONSUMER_KEY = "9C7552E1-3C04-4D04-A395-230D8931E494";
+	private static final String OAUTH_CONSUMER_SECRET = "DA7CC147-7B5B-4423-BCB4-D0C03E2BF685";
 
-	// please use your own callback url if you want capture oauth_verifier parameter
-	public static final String OAUTH_CALLBACK_URL = "http://oauth.callback/callback";
+    // please use your own callback url if you want capture oauth_verifier parameter
+    private static final String OAUTH_CALLBACK_URL = "http://test.arcao.com/oauth.php";
 
-	public final static String GEOCACHE_CODE = "GCY81P";
+    private final static String GEOCACHE_CODE = "GCY81P";
 
-	private static OAuthService createOAuthService() {
-		ServiceBuilder serviceBuilder = new ServiceBuilder()
-						.apiKey(OAUTH_CONSUMER_KEY)
-						.apiSecret(OAUTH_CONSUMER_SECRET)
-						.callback(OAUTH_CALLBACK_URL)
-						.debug();
+    private GeocachingMain() {
+    }
 
-		// For staging server has to be used staging OAuth service
-		serviceBuilder.provider(GeocachingOAuthProvider.Staging.class);
+    private static OAuth10aService createOAuthService() {
+        ServiceBuilder serviceBuilder = new ServiceBuilder()
+                .apiKey(OAUTH_CONSUMER_KEY)
+                .apiSecret(OAUTH_CONSUMER_SECRET)
+                .callback(OAUTH_CALLBACK_URL)
+                .debug();
 
-		return serviceBuilder.build();
-	}
+        // For staging server has to be used staging OAuth service
+        return serviceBuilder.build(new GeocachingOAuthProvider.Staging());
+    }
 
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException, GeocachingApiException {
 
-		OAuthService oAuthService = createOAuthService();
+        OAuth10aService service = createOAuthService();
 
-		System.out.println("Fetching request token from Geocaching...");
+        System.out.println("Fetching request token from Geocaching...");
 
-		// Groundspeak OAuth service use OAuth 1.0a, so you must always specify your own callback URL
-		final Token requestToken = oAuthService.getRequestToken();
+        // Groundspeak OAuth service use OAuth 1.0a, so you must always specify your own callback URL
+        OAuth1RequestToken requestToken = service.getRequestToken();
 
-		System.out.println("Request token: " + requestToken.getToken());
-		System.out.println("Token secret: " + requestToken.getSecret());
+        System.out.println("Request token: " + requestToken.getToken());
+        System.out.println("Token secret: " + requestToken.getTokenSecret());
 
-		String authUrl = oAuthService.getAuthorizationUrl(requestToken);
+        String authUrl = service.getAuthorizationUrl(requestToken);
 
-		System.out.println("Now visit:\n" + authUrl + "\n... and grant this app authorization");
+        System.out.println("Now visit:\n" + authUrl + "\n... and grant this app authorization");
 
-		// after you allow the access you'll get move to URL like (callback url + authorization parameters):
-		// e.g.: http://oauth.callback/callback?oauth_verifier=...&oauth_token=...
-		// put oauth_verifier bellow (beware! must be URL decoded)
-		System.out.println("Enter the oauth_verifier and hit ENTER when you're done:");
+        // after you allow the access you'll get move to URL like (callback url + authorization parameters):
+        // e.g.: http://oauth.callback/callback?oauth_verifier=...&oauth_token=...
+        // put oauth_verifier bellow (beware! must be URL decoded)
+        System.out.println("Enter the oauth_verifier and hit ENTER when you're done:");
 
-		final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		final String oAuthVerifier = br.readLine();
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        final String oAuthVerifier = br.readLine();
 
-		System.out.println("Fetching access token from Geocaching...");
+        System.out.println("Fetching access token from Geocaching...");
 
-		Token accessToken = oAuthService.getAccessToken(requestToken, new Verifier(oAuthVerifier));
+        OAuth1AccessToken accessToken = service.getAccessToken(requestToken, oAuthVerifier);
 
-		System.out.println("Access token: " + accessToken.getToken());
-		System.out.println("Token secret: " + accessToken.getSecret());
+        System.out.println("Access token: " + accessToken.getToken());
+        System.out.println("Token secret: " + accessToken.getTokenSecret());
 
-		// create Geocaching API instance
-		final GeocachingApi api = LiveGeocachingApi.Builder.liveGeocachingApi()
+        // create Geocaching API instance
+        final GeocachingApi api = LiveGeocachingApi.builder()
                 // use staging configuration (mean staging service url)
-						.withConfiguration(new DefaultStagingGeocachingApiConfiguration())
+                .configuration(new DefaultStagingGeocachingApiConfiguration())
                 .build();
 
-		// now for Geocaching API use access token as a session token
-		// Note: Save this token for later usage, it wouldn't have expire. Token can expire only
-		//       in case user forbid your application on Geocaching site.
-		api.openSession(accessToken.getToken());
+        // now for Geocaching API use access token as a session token
+        // Note: Save this token for later usage, it wouldn't have expire. Token can expire only
+        //       in case user forbid your application on Geocaching site.
+        api.openSession(accessToken.getToken());
 
-		System.out.println("Sending request to Geocaching...");
+        System.out.println("Sending request to Geocaching...");
 
-		// get a geocache
-		final Geocache cache = api.getGeocache(GeocachingApi.ResultQuality.LITE, GEOCACHE_CODE, 0, 0);
+        // get a geocache
+        final Geocache cache = api.getGeocache(GeocachingApi.ResultQuality.LITE, GEOCACHE_CODE, 0, 0);
 
-		System.out.println("Response: " + cache.toString());
-	}
+        System.out.println("Response: " + cache);
+    }
 }
--- oauth/src/main/java/com/arcao/geocaching/api/example/oauth/provider/GeocachingOAuthProvider.java
+++ oauth/src/main/java/com/arcao/geocaching/api/example/oauth/provider/GeocachingOAuthProvider.java
@@ -1,53 +1,87 @@
 package com.arcao.geocaching.api.example.oauth.provider;
 
-import com.arcao.geocaching.api.example.oauth.provider.services.CustomTokenExtractorImpl;
-import org.scribe.builder.api.DefaultApi10a;
-import org.scribe.extractors.AccessTokenExtractor;
-import org.scribe.extractors.RequestTokenExtractor;
-import org.scribe.model.Token;
-import org.scribe.services.TimestampService;
-import org.scribe.utils.OAuthEncoder;
+import com.github.scribejava.core.builder.api.DefaultApi10a;
+import com.github.scribejava.core.exceptions.OAuthException;
+import com.github.scribejava.core.extractors.OAuth1AccessTokenExtractor;
+import com.github.scribejava.core.extractors.OAuth1RequestTokenExtractor;
+import com.github.scribejava.core.extractors.TokenExtractor;
+import com.github.scribejava.core.model.OAuth1AccessToken;
+import com.github.scribejava.core.model.OAuth1RequestToken;
+import com.github.scribejava.core.utils.OAuthEncoder;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class GeocachingOAuthProvider extends DefaultApi10a {
-  private static final String OAUTH_URL = "https://www.geocaching.com/oauth/mobileoauth.ashx";
-
-  private TimestampService timestampService = null;
-
-  @Override
-  public RequestTokenExtractor getRequestTokenExtractor() {
-    return new CustomTokenExtractorImpl();
-  }
-
-  @Override
-  public AccessTokenExtractor getAccessTokenExtractor() {
-    return new CustomTokenExtractorImpl();
-  }
-
-  protected String getOAuthUrl() {
-    return OAUTH_URL;
-  }
-
-  @Override
-  public String getRequestTokenEndpoint() {
-    return getOAuthUrl();
-  }
-
-  @Override
-  public String getAccessTokenEndpoint() {
-    return getOAuthUrl();
-  }
-
-  @Override
-  public String getAuthorizationUrl(Token requestToken) {
-    return getOAuthUrl() + "?oauth_token=" + OAuthEncoder.encode(requestToken.getToken());
-  }
-
-  public static class Staging extends GeocachingOAuthProvider {
-    private static final String OAUTH_URL = "https://staging.geocaching.com/oauth/mobileoauth.ashx";
+    private static final Pattern ERROR_MESSAGE_REGEX = Pattern.compile("oauth_error_message=([^&]*)");
+    private static final String OAUTH_URL = "https://www.geocaching.com/oauth/mobileoauth.ashx";
 
     @Override
-    protected String getOAuthUrl() {
-      return OAUTH_URL;
+    public TokenExtractor<OAuth1RequestToken> getRequestTokenExtractor() {
+        return new GeocachingRequestTokenExtractor();
     }
-  }
+
+    @Override
+    public TokenExtractor<OAuth1AccessToken> getAccessTokenExtractor() {
+        return new GeocachingAccessTokenExtractor();
+    }
+
+    protected String getOAuthUrl() {
+        return OAUTH_URL;
+    }
+
+    @Override
+    public String getRequestTokenEndpoint() {
+        return getOAuthUrl();
+    }
+
+    @Override
+    public String getAccessTokenEndpoint() {
+        return getOAuthUrl();
+    }
+
+    @Override
+    public String getAuthorizationUrl(OAuth1RequestToken requestToken) {
+        return getOAuthUrl() + "?oauth_token=" + OAuthEncoder.encode(requestToken.getToken());
+    }
+
+    static void checkError(CharSequence response) {
+        Matcher matcher = ERROR_MESSAGE_REGEX.matcher(response);
+        if (matcher.find() && matcher.groupCount() >= 1) {
+            throw new OAuthException(OAuthEncoder.decode(matcher.group(1)));
+        }
+    }
+
+    public static class Staging extends GeocachingOAuthProvider {
+        private static final String OAUTH_URL = "https://staging.geocaching.com/oauth/mobileoauth.ashx";
+
+        @Override
+        protected String getOAuthUrl() {
+            return OAUTH_URL;
+        }
+    }
+
+    private static class GeocachingRequestTokenExtractor extends OAuth1RequestTokenExtractor {
+        GeocachingRequestTokenExtractor() {
+        }
+
+        @Override
+        public OAuth1RequestToken extract(String response) {
+            if (response != null)
+                checkError(response);
+            return super.extract(response);
+        }
+    }
+
+    private static class GeocachingAccessTokenExtractor extends OAuth1AccessTokenExtractor {
+        GeocachingAccessTokenExtractor() {
+        }
+
+        @Override
+        public OAuth1AccessToken extract(String response) {
+            if (response != null)
+                checkError(response);
+            return super.extract(response);
+        }
+    }
 }
